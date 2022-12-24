745df301319465841f8b0a83f921c018a20005e3
Daniel Trushkov <98241364+TruDan12345@users.noreply.github.com>
2022-12-24 10:42:02 -0800

    Removed Delay

--- comtictactoe.cpp
+++ comtictactoe.cpp
@@ -11,14 +11,6 @@
 
 using namespace std;
 
-/**
-    using namespace std::this_thread; // sleep_for, sleep_until
-    using namespace std::chrono;
-    sleep_for(nanoseconds(10));
-    sleep_until(system_clock::now() + seconds(1));
-    code snippet for system time delay
-**/
-
 int sd;
 class main_board
 {
@@ -130,11 +122,7 @@ public:
     }
     void o_move(int &sd) // computer decision part of the game
     {
-        using namespace std::this_thread; // sleep_for, sleep_until
-        using namespace std::chrono;
-        sleep_for(nanoseconds(10));
-        sleep_until(system_clock::now() + seconds(1));
-        
+
         dont_apply = true;
         for (int a = 0; a < 3; a++)
         {
